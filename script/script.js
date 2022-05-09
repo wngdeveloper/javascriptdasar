5682e1141c8dbb254e0ad8199a683ff0566bba60
wngdeveloper <wngdeveloper@gmail.com>
2022-05-09 07:45:05 +0700

    first commit

---
+++ script/script.js
@@ -0,0 +1,37 @@
+document.writeln("TIPE DATA");
+document.writeln("</br>");
+document.writeln(100 + ' | Ini adalah tipe data angka'); //angka
+document.writeln("</br>");
+document.writeln(100.100 + ' | Ini adalah tipe data desimal') //desimal
+document.writeln("</br>");
+document.writeln(0b11010010); //binnary
+document.writeln("</br>");
+
+document.writeln(true); //tipe data boolean
+document.writeln("</br>");
+document.writeln(false);
+document.writeln("</br>");
+document.writeln("</br>");
+
+//TIPE DATA STRING
+//tipe data string bisa menggunakan '' ataupun "", yang penting konsisten
+
+// ESCAPE SEQUENCE
+document.writeln("ESCAPE SEQUENCE\n");
+document.writeln("</br>");
+document.writeln("<textarea cols='100' rows='10'>");
+document.writeln("Anggit \nPuguh \nSetyawan\n");
+document.writeln("\\Wonokarto\\WOnogiri\\\n");
+document.writeln("\'Fullstack Programmer\'\n");
+document.writeln("\"Belajar Javascript\"\n");
+document.writeln("Mei 2022");
+document.writeln("</textarea>");
+document.writeln("</br>");
+document.writeln("</br>");
+document.writeln("</br>");
+
+//VARIABLE
+document.writeln("VARIABLE\n");
+document.writeln("</br>");
+
+//tipe variable hanya bisa 2, yaitu let atau const. kalau let masih bisa diubah-ubah, kalau const tidak bisa diubah
