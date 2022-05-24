56b796d947e7c249f38f4ac8661786d7b7325de6
wngdeveloper <wngdeveloper@gmail.com>
2022-05-25 06:46:51 +0700

    25 mei 2022

---
+++ leetcode/concatenation-of-array.js
@@ -0,0 +1,18 @@
+// let nums = [1,2,3,6,7,8];
+
+// var getConcatenation = function(nums){
+// //copy Array
+// //concate Array
+
+// let newArr = [...nums];
+// return newArr.concat(nums);
+
+// };
+
+// console.log(getConcatenation(nums));
+
+
+numbers = [1, 2, 3];
+double = (x) => x * 2;
+
+console.log(numbers.map(double));
