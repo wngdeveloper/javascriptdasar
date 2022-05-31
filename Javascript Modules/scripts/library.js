4040d16e88887f16def90add10222d00c820a4b1
wngdeveloper <wngdeveloper@gmail.com>
2022-05-31 15:27:07 +0700

    Javascript Model

---
+++ Javascript Modules/scripts/library.js
@@ -0,0 +1,7 @@
+function sayHello(name){
+    console.log(`Hello ${name}`);
+}
+
+function sayGoodBye(name){
+    console.log(`Good Bye ${name}`);
+}
---
+++ Javascript Modules/scripts/say.js
@@ -0,0 +1,9 @@
+export function sayHello(name){
+    console.log(`Hello ${name}`);
+}
+
+export function sayGoodBye(name){
+    console.log(`Good Bye ${name}`);
+}
+
+export const name = "setyawan";
