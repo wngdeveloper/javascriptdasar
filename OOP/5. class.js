72f4568070392717d07e85872fa639be29adb9e9
wngdeveloper <wngdeveloper@gmail.com>
2022-06-03 05:50:28 +0700

    oop 5:50

---
+++ OOP/5. class.js
@@ -0,0 +1,10 @@
+{
+//Membuat Class
+    class Person{
+        firstName = "Anggit";
+        lastName = "Setyawan";
+    }
+
+    const eko = new Person(); //membuat ocject dari class
+    console.log(`${eko.firstName}`);
+}
