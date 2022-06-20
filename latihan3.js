b9a0b5b9334beffda18b8b58932535b840a85015
wngdeveloper <wngdeveloper@gmail.com>
2022-06-21 00:48:27 +0700

    coba

--- latihan3.js
+++ latihan3.js
@@ -1,10 +1,58 @@
-let a;
-let b;
-let c;
 
-for(a=5; a>=0; a--){
-    for(b=0;b<a;b++){
-            console.log("*");
+// let hasil='';
+
+// for (let i = 5; i > 0 ; i--){
+//     for (let j = 0; j < i; j++){
+//         hasil += ' *';
+//     }
+//     hasil += '\n';
+// }
+
+// console.log(`${hasil}`);
+
+
+// ###################
+// let hasil = '';
+
+// for (let i = 5; i > 0; i--){
+//     for (let j = 5; j > 0; j--){
+//         if ( j>i ){
+//             hasil += '';
+//         } else {
+//             hasil += '* ';
+//         }
+//     }
+//     hasil += '\n';
+// }
+
+// console.log(`${hasil}`);
+
+
+// for (var i=1;i<=5;i++){
+//     for (var j=1;j<=i;j++){
+//         // document.write("&nbsp&nbsp");
+//         console.log("_");
+//     }
+//     for (var k=i;k<=5;k++){
+//         console.log("*");
+//     }
+//     console.log("\n>");
+// }
+function segitiga1(panjang) {
+    let hasil = '';
+    for (let i = panjang; i >0; i--) {
+        for (let j = 0; j <= i; j++) {
+            hasil += '* ';
+        }
+        hasil += '\n';
     }
-    console.log("");
+    return hasil;
 }
+console.log(segitiga1(5));
+
+
+let bintang = '';
+for (let a=1; a<=7; a++){
+    bintang += '* ';
+}
+console.log(bintang);
